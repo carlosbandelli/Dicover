9f5f8e79266e6b4e83306dcee761b7350e39d776
Carlos Bandelli <79877865+carlosbandelli@users.noreply.github.com>
2023-08-17 15:21:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 import express from "express";
+import axios from "axios";
 
 const app = express();
-
 app.listen("3000");
 
-app.route("/").get((req, res) => res.send("hello"));
+app.use(express.json());
+
+let author = "Carlos";
+
+app.route("/").get((req, res) =>
+  axios
+    .get("https://api.github.com/users/carlosbandelli")
+    .then((result) => res.send(`<img src="${result.data.avatar_url}"/>`))
+    .catch((error) => console.error(error))
+);
+
+app.route("/:parametro").get((req, res) => res.send(req.params.parametro));
+
+app.route("/").post((req, res) => res.send(req.body));
+
+app.route("/").put((req, res) => {
+  author = req.body.author;
+  res.send(author);
+});
+
+app
+  .route("/:identificador")
+  .delete((req, res) => res.send(req.params.identificador));
+
+app.route("/about/user").get((req, res) => res.send(req.query.id));
